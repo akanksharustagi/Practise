1dadf2dac116a74afcc4c8f39449258f137c9b12
akanksharustagi <ena.akanksha@gmail.com>
2022-05-21 22:10:34 -0400

    added formula

---
+++ src/main/java/Cube.java
@@ -0,0 +1,4 @@
+public class Cube {
+    public static int cube(int n){
+        return (n*n*n);    }
+}
---
+++ src/test/java/AllTest.java
@@ -0,0 +1,29 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AllTest {
+    Calculator calculator;
+    @Before
+    public void Setup(){
+        calculator= new Calculator();
+    }
+    @Test
+    public void sumTest(){
+        int expect = 6;
+        int outcome= calculator.sum(2,4);
+        Assert.assertEquals(expect, outcome);
+    }
+    @Test
+    public void sumTest2(){
+        int expected = -2;
+        int outcome = calculator.sum(2 ,-4);
+        Assert.assertEquals(expected, outcome);
+    }
+@Test
+    public void cubeTest(){
+        int expectations  = -8;
+        int output = Cube.cube(2);
+        Assert.assertEquals(expectations, output);
+}
+}
